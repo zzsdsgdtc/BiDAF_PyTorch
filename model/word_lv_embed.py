f53d9e29524fab0ef60dff9655c832d70e95e4fd
Jason Zhang <zishengzhang@JasondeMacBook-Pro.local>
2018-05-21 15:13:09 -0700

    fix name

--- model/word_lv_embed.py
+++ model/word_lv_embed.py
@@ -6,7 +6,7 @@ import torch.optim as optim
 class WordEmbed(nn.Module):
 	def __init__(self, args):
 		super(WordEmbed, self).__init__()
-		self.embedding = nn.Embedding(args.num_of_word, args.embd_dim)
+		self.embedding = nn.Embedding(args.word_vocab_size, args.word_embd_dim)
 		if args.pretrained:
 			self.embedding.weight = nn.Parameter(args.pretrained_embd, requires_grad = False)
 
