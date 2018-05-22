19d17add2d1896da99466cff26fc35911cd344cc
Jason Zhang <zishengzhang@JasondeMacBook-Pro.local>
2018-05-21 18:32:01 -0700

    add flatten_parameters()

--- model/BiDAF.py
+++ model/BiDAF.py
@@ -28,7 +28,8 @@ class BiDAF(nn.Module):
         word_embding = self.word_embd_model(word_lv)
         concat = torch.cat((char_embding, word_embding), 2)
         highway_embding = self.highway_model(concat)
-        ctx_embd, _h = self.ctx_embd_model(highway_embding)
+        self.ctx_embd_model.flatten_parameters()
+        ctx_embd, _ = self.ctx_embd_model(highway_embding)
         ctx_embd = self.dropout(ctx_embd)
         return ctx_embd
         
@@ -43,13 +44,15 @@ class BiDAF(nn.Module):
         G = self.attn_embd_model(ctx_embding, query_embding)
         
         # 5. Modeling Layer
-        M, _h = self.modeling(G)
+        self.modeling.flatten_parameters()
+        M, _ = self.modeling(G)
             
         # 6. Output Layer
         concat_GM = torch.cat((G, M), 2)
         p1 = F.softmax(self.startIdx(concat_GM).squeeze(), dim = -1)
         
-        M2, _h = self.endIdx_lstm(M)
+        self.endIdx_lstm.flatten_parameters()
+        M2, _ = self.endIdx_lstm(M)
         M2 = self.dropout(M2)
         concat_GM2 = torch.cat((G, M2), 2)
         p2 = F.softmax(self.endIdx(concat_GM2).squeeze(), dim = -1)        
